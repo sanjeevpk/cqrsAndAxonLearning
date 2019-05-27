e93b820da3285b01e12fbf3bb771e1a0d6c44d44
Sanjeev Kulkarni <sanjeev.pk@gmail.com>
2019-05-27 10:08:41 -0700

    Replaced my implementation

---
+++ src/main/java/com/sanjeev/cqrsAndAxonLearning/CqrsAndAxonLearningApplication.java
@@ -0,0 +1,13 @@
+package com.sanjeev.cqrsAndAxonLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsAndAxonLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsAndAxonLearningApplication.class, args);
+	}
+
+}
